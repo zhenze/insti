d89770b386fa232b6e18238320856f18a6936b8c
zhenze <zhenze@gmail.com>
2015-11-24 19:12:03 +0100

    Update YearUtilities.java
    perfecta

--- insti/src/YearUtilities.java
+++ insti/src/YearUtilities.java
@@ -5,17 +5,17 @@ public class YearUtilities {
 	        if(year%4 == 0){
 	            if(year%100 == 0){
 	                if(year%400 == 0){
-	                    return false; 
+	                    return true; 
 	                }else{
-	                    return true;  
+	                    return false;  
 	                }
 	            }else{
-	                return false;
+	                return true;
 	            }
 	        }else{
-	            return true;
+	            return false;
 	        }
 	    }
-	
+		
 
 }
